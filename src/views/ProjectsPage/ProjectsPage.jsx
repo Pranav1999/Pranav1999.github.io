a0e8fcc5e9eaaed3a7c4123a2f3b7f28ef96ca92
pranav0281999 <pranav0281999@gmail.com>
2021-06-05 23:25:06 +0530

    Updates Projects

--- src/views/ProjectsPage/ProjectsPage.jsx
+++ src/views/ProjectsPage/ProjectsPage.jsx
@@ -14,10 +14,6 @@ import GridItem from "components/Grid/GridItem.jsx";
 import HeaderLinks from "components/Header/HeaderLinks.jsx";
 import NavPills from "components/NavPills/NavPills.jsx";
 import Parallax from "components/Parallax/Parallax.jsx";
-//icons
-import FileCopy from "@material-ui/icons/FileCopy";
-
-import profile from "assets/img/faces/pranav.jpg";
 
 import profilePageStyle from "assets/jss/material-kit-react/views/profilePage.jsx";
 
@@ -25,126 +21,118 @@ import profilePageStyle from "assets/jss/material-kit-react/views/profilePage.js
 import Card from "components/Card/Card.jsx";
 import CardBody from "components/Card/CardBody.jsx";
 
-import imagesStyles from "assets/jss/material-kit-react/imagesStyles.jsx";
-
-import { cardTitle } from "assets/jss/material-kit-react.jsx";
+import gcInteriorImage from "../../assets/img/projects/GCInteriors.png";
+import audioReactiveTunnelImage from "../../assets/img/projects/AudioReactiveTunnel.jpeg";
+import deflectThatBallImage from "../../assets/img/projects/DeflectThatBall.jpeg";
+import flappyHumanImage from "../../assets/img/projects/FlappyHuman.jpeg";
+import reelWorldImage from "../../assets/img/projects/ReelWorld.png";
 
 var projects = [
-  {
-    name: "UglyChatApp",
-    description:
-      "An attempt to create a messaging application similar to the popular ones using a XMPP server and a NodeJS backend without focusing much on the UI design.",
-    image: require("assets/img/projects/ugly_chat_app.png"),
-    link: "https://github.com/pranav0281999/UglyChatApp"
-  },
-  {
-    name: "BioTrips",
-    description:
-      "Works as a travel buddy for drivers, who can manage trips allocated to them, and allows vendors to make quick decisions with real time info about trips and drivers.",
-    image: require("assets/img/projects/biotrips.png"),
-    link: "https://angel.co/projects/1008717-biotrips"
-  },
-  {
-    name: "Content Talks",
-    description:
-      "Lets you share any type of document in HTML format and observe how individuals interact with your document in form of clicks, pointer movement, and scrolls.",
-    image: require("assets/img/projects/content_talks.png"),
-    link: "https://angel.co/projects/1009433-content-talks"
-  },
-  {
-    name: "SimFa",
-    description:
-      "A simple face recognition app that lets you capture an image of a person or person's image and recognize that person in real time using that single image.",
-    image: require("assets/img/projects/simfa.png"),
-    link: "https://angel.co/projects/1009407-simfa"
-  },
-  {
-    name: "Poshan",
-    description:
-      "A mobile application developed as a step towards helping the malnourished and underserved children of today to become the healthy youth of tomorrow.",
-    image: require("assets/img/projects/poshan.jpeg"),
-    link: "https://angel.co/projects/920194-poshan"
-  },
-  {
-    name: "AmazonPriceTraqueur",
-    description:
-      "A python web scrapper that can track prices of multiple products and can send an email whenever price of selected product drops below a threshold.",
-    image: require("assets/img/projects/amazon.png"),
-    link: "https://github.com/Pranav1999/AmazonPriceTraqueur/"
-  }
+    {
+        name: "GC Interiors Portfolio",
+        description:
+            "React.js based responsive portfolio website for an Interior Design professional with pinch zoom and virtual reality 360° image viewer.",
+        image: gcInteriorImage,
+        link: "https://geetuchaurasiya.netlify.app/"
+    },
+    {
+        name: "Reel World",
+        description:
+            "A web based video conferencing app within a virtual world, brings online meeting experience closer to physical meetings.",
+        image: reelWorldImage,
+        link: "https://github.com/pranav0281999/ReelWorld"
+    },
+    {
+        name: "Deflect That Ball",
+        description:
+            "Experimentation with Processing and Xbox Kinect V2 to create a game of deflecting the virtual balls so that they don't touch the ground.",
+        image: deflectThatBallImage,
+        link: "https://github.com/pranav0281999/ProcessingKinectExperiments/tree/master/DeflectThatBall"
+    },
+    {
+        name: "Flappy Human Image",
+        description:
+            "Experimentation with Processing and Xbox Kinect V2 to create Flappy Birds like game but with human and motion detection.",
+        image: flappyHumanImage,
+        link: "https://github.com/pranav0281999/ProcessingKinectExperiments/tree/master/FlappyHuman"
+    },
+    {
+        name: "Audio Reactive Tunnel",
+        description:
+            "Experimentation with three.js where intensity of tunnel lights increases with increasing average frequency of background audio.",
+        image: audioReactiveTunnelImage,
+        link: "https://github.com/pranav0281999/AudioReactiveTunnel"
+    },
 ];
 
 class ProfilePage extends React.Component {
-  render() {
-    const { classes, ...rest } = this.props;
-    const navImageClasses = classNames(classes.imgRounded, classes.imgGallery);
-    
-    return (
-      <div>
-        <Header
-          color="transparent"
-          brand="Pranav"
-          rightLinks={<HeaderLinks />}
-          fixed
-          changeColorOnScroll={{
-            height: 200,
-            color: "white"
-          }}
-          {...rest}
-        />
-        <Parallax small filter image={require("assets/img/projects-bg.jpg")} />
-        <div className={classNames(classes.main, classes.mainRaised)}>
-          <div>
-            <div className={classes.container}>
-              <GridContainer justify="center">
-                {projects.map(project => {
-                  console.log("assets/img/projects/biotrips.png");
-                  console.log(project.image);
-                  return (
-                    <div style={{ justifyContent: "center" }}>
-                      <GridItem xs={12} sm={12} md={4}>
-                        <Card style={{ width: "15rem" }}>
-                          <img
-                            style={{
-                              height: "220px",
-                              width: "220px",
-                              display: "block",
-                              alignSelf: "center"
-                            }}
-                            className={classes.imgCardTop}
-                            src={project.image}
-                            alt="Card-img-cap"
-                          />
-                          <CardBody>
-                            <h4 className={classes.cardTitle}>
-                              {project.name}
-                            </h4>
-                            <p>{project.description}</p>
-                            <Button
-                              color="dark"
-                              href={project.link}
-                              target="_blank"
-                            >
-                              Know More
-                            </Button>
-                          </CardBody>
-                        </Card>
-                      </GridItem>
+    render() {
+        const {classes, ...rest} = this.props;
+
+        return (
+            <div>
+                <Header
+                    color="transparent"
+                    brand="Pranav"
+                    rightLinks={<HeaderLinks/>}
+                    fixed
+                    changeColorOnScroll={{
+                        height: 200,
+                        color: "white"
+                    }}
+                    {...rest}
+                />
+                <Parallax small filter image={require("assets/img/projects-bg.jpg")}/>
+                <div className={classNames(classes.main, classes.mainRaised)}>
+                    <div>
+                        <div className={classes.container}>
+                            <GridContainer justify="center">
+                                {projects.map(project => {
+                                    return (
+                                        <div style={{justifyContent: "center"}}>
+                                            <GridItem xs={12} sm={12} md={4}>
+                                                <Card style={{width: "15rem"}}>
+                                                    <img
+                                                        style={{
+                                                            height: "220px",
+                                                            width: "220px",
+                                                            display: "block",
+                                                            alignSelf: "center"
+                                                        }}
+                                                        className={classes.imgCardTop}
+                                                        src={project.image}
+                                                        alt="Card-img-cap"
+                                                    />
+                                                    <CardBody>
+                                                        <h4 className={classes.cardTitle}>
+                                                            {project.name}
+                                                        </h4>
+                                                        <p>{project.description}</p>
+                                                        <Button
+                                                            color="dark"
+                                                            href={project.link}
+                                                            target="_blank"
+                                                        >
+                                                            Know More
+                                                        </Button>
+                                                    </CardBody>
+                                                </Card>
+                                            </GridItem>
+                                        </div>
+                                    );
+                                })}
+                            </GridContainer>
+                        </div>
                     </div>
-                  );
-                })}
-              </GridContainer>
+                </div>
+                <Footer/>
             </div>
-          </div>
-        </div>
-        <Footer />
-      </div>
-    );
-  }
+        );
+    }
 }
 
 ProfilePage.propTypes = {
-  classes: PropTypes.object
+    classes: PropTypes.object
 };
 
 export default withStyles(profilePageStyle)(ProfilePage);
