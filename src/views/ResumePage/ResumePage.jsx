4b317244c35f6ba2067888b900497ca7bde36b6c
pranav0281999 <pranav0281999@gmail.com>
2021-06-05 21:34:41 +0530

    Updates Resume

--- src/views/ResumePage/ResumePage.jsx
+++ src/views/ResumePage/ResumePage.jsx
@@ -12,96 +12,89 @@ import Button from "components/CustomButtons/Button.jsx";
 import GridContainer from "components/Grid/GridContainer.jsx";
 import GridItem from "components/Grid/GridItem.jsx";
 import HeaderLinks from "components/Header/HeaderLinks.jsx";
-import NavPills from "components/NavPills/NavPills.jsx";
 import Parallax from "components/Parallax/Parallax.jsx";
 
 import profilePageStyle from "assets/jss/material-kit-react/views/profilePage.jsx";
 
-// core components
-import Card from "components/Card/Card.jsx";
-import CardBody from "components/Card/CardBody.jsx";
+import {PictureAsPdf} from "@material-ui/icons";
 
-import imagesStyles from "assets/jss/material-kit-react/imagesStyles.jsx";
-
-import { cardTitle } from "assets/jss/material-kit-react.jsx";
-import { PictureAsPdf } from "@material-ui/icons";
+import resumeImage from "../../assets/img/content/Pranav's Resume.jpg"
+import resumeIconImage from "../../assets/img/content/resume-icon-black.png"
+import resumePDF from "../../assets/docs/Pranav's Resume.pdf"
+import backgroundImage from "../../assets/img/resume-bg.jpg"
 
 class ProfilePage extends React.Component {
-  render() {
-    const { classes, ...rest } = this.props;
-    const imageClasses = classNames(
-      classes.imgRaised,
-      classes.imgRoundedCircle,
-      classes.imgFluid
-    );
-    const navImageClasses = classNames(classes.imgRounded, classes.imgGallery);
-    return (
-      <div>
-        <Header
-          color="transparent"
-          brand="Pranav"
-          rightLinks={<HeaderLinks />}
-          fixed
-          changeColorOnScroll={{
-            height: 200,
-            color: "black"
-          }}
-          {...rest}
-        />
-        <Parallax small filter image={require("assets/img/resume-bg.jpg")} />
-        <div className={classNames(classes.main, classes.mainRaised)}>
-          <div>
-            <div className={classes.container}>
-              <br />
-              <br />
-              <GridContainer justify="center">
-                <GridItem xs={12} sm={12} md={6}>
-                  <div
-                    style={{
-                      display: "flex",
-                      justifyContent: "center",
-                      alignItems: "center",
-                      flexDirection: "column",
-                      height: "100%",
-                      marginBottom: 50
+    render() {
+        const {classes, ...rest} = this.props;
+        return (
+            <div>
+                <Header
+                    color="transparent"
+                    brand="Pranav"
+                    rightLinks={<HeaderLinks/>}
+                    fixed
+                    changeColorOnScroll={{
+                        height: 200,
+                        color: "black"
                     }}
-                  >
-                    <img
-                      src={require("assets/img/content/resume-icon-black.png")}
-                      style={{ height: 100, width: 100 }}
-                    />
-                    <br />
-                    <h2>RESUME</h2>
-                    <br />
-                    <Button
-                      color="dark"
-                      href={require("assets/docs/Pranav_Bhardwaj_Resume.pdf")}
-                    >
-                      <PictureAsPdf />
-                      Download Resume
-                    </Button>
-                  </div>
-                </GridItem>
-                <GridItem xs={12} sm={12} md={6}>
-                  <img
-                    src={require("assets/img/content/resume.jpg")}
-                    className={classes.imgRaised + " " + classes.imgFluid}
-                  />
-                </GridItem>
-              </GridContainer>
-              <br />
-              <br />
+                    {...rest}
+                />
+                <Parallax small filter image={backgroundImage}/>
+                <div className={classNames(classes.main, classes.mainRaised)}>
+                    <div>
+                        <div className={classes.container}>
+                            <br/>
+                            <br/>
+                            <GridContainer justify="center">
+                                <GridItem xs={12} sm={12} md={6}>
+                                    <div
+                                        style={{
+                                            display: "flex",
+                                            justifyContent: "center",
+                                            alignItems: "center",
+                                            flexDirection: "column",
+                                            height: "100%",
+                                            marginBottom: 50
+                                        }}
+                                    >
+                                        <img
+                                            src={resumeIconImage}
+                                            style={{height: 100, width: 100}}
+                                            alt={"Resume Icon"}
+                                        />
+                                        <br/>
+                                        <h2>RESUME</h2>
+                                        <br/>
+                                        <Button
+                                            color="dark"
+                                            href={resumePDF}
+                                        >
+                                            <PictureAsPdf/>
+                                            Download Resume
+                                        </Button>
+                                    </div>
+                                </GridItem>
+                                <GridItem xs={12} sm={12} md={6}>
+                                    <img
+                                        src={resumeImage}
+                                        className={classes.imgRaised + " " + classes.imgFluid}
+                                        alt={"Resume"}
+                                    />
+                                </GridItem>
+                            </GridContainer>
+                            <br/>
+                            <br/>
+                        </div>
+                    </div>
+                </div>
+                <Footer/>
             </div>
-          </div>
-        </div>
-        <Footer />
-      </div>
-    );
-  }
+        );
+    }
 }
 
 ProfilePage.propTypes = {
-  classes: PropTypes.object
+    classes: PropTypes.object
 };
 
 export default withStyles(profilePageStyle)(ProfilePage);
